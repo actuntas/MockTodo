c0c79175065061c66f6445ad299a94cd76f7cee3
cantuntas <Actuntas@gmail.com>
2024-02-15 04:59:40 +0300

    Initial Commit

---
+++ MockTodo/MockTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockTodoApp.swift
+//  MockTodo
+//
+//  Created by Can Tuntaş on 15.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct MockTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
